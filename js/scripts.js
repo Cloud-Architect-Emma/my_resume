e38472e7228ded845b17e588ef3bb1d334a9a9f1
EMMANUELA OPURUM <86380966+Cloud-Architect-Emma@users.noreply.github.com>
2025-10-14 23:13:56 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
+/*!
+* Custom Scripts for Emmanuela Opurum Resume Page
+*/
+
+// Wait for the DOM to fully load
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the side navigation
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    }
+
+    // Collapse responsive navbar when a nav item is clicked
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.forEach(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
 
