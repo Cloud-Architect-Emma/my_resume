d5b764431befe7ac0a424b1f528aa3c0120c908b
EMMANUELA OPURUM <86380966+Cloud-Architect-Emma@users.noreply.github.com>
2025-10-15 10:56:24 +0100

    Resume scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 /*!
-* Custom Scripts for Emmanuela Opurum Resume Page
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
+//
+// Scripts
+// 
 
-// Wait for the DOM to fully load
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the side navigation
+    // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
             rootMargin: '0px 0px -40%',
         });
-    }
+    };
 
-    // Collapse responsive navbar when a nav item is clicked
+    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.forEach(function (responsiveNavItem) {
+    responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
@@ -28,4 +32,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
